52cbd80d4b605df57a0de653b6def5f45113f83e
Bluscream <bluscream@outlook.de>
2021-06-19 05:37:22 +0200

    added vehicle shop generator (hopefully lol)

--- folderScanner/__init__.py
+++ folderScanner/__init__.py
@@ -109,14 +109,14 @@ class ResourceScanner(object):
             copyfile("cache/non-els.xml", target)
 
     def generateVehicleShopSQL(self, targetPath: Path, dbName: str, namePattern: str = "{category}", tableName: str = "vehicles", defaultPrice: int = 1000000):
-        with open(targetPath, 'w') as f: # ("vehicle_shop.sql"
+        with open(targetPath, 'w') as f:
             f.write(f"-- GENERATED AT {datetime.now()} BY https://github.com/Bluscream/FiveMResourceScanner{linesep}")
             f.write(f"-- GENERATED FROM {self.counts.spawnables} Spawnables | {self.counts.resources} Resources | {self.counts.categories} Categories | {self.counts.directories} Folders{linesep}")
             f.write(f"USE `{dbName}`{linesep}{linesep}")
             for resource in self.resources:
                 for spawnname in resource.spawnnames:
                     f.write(f"""BEGIN
-   IF NOT EXISTS (SELECT * FROM `{tableName}` WHERE `model` = `{spawnname}`)
+   IF NOT EXISTS (SELECT * FROM `{tableName}` WHERE `model` = "{spawnname}")
    BEGIN
        INSERT INTO `{tableName}` (`name`, `model`, `price`, `category`) VALUES ("{namePattern.format(model=spawnname, resource=resource.name, category=resource.category)}", "{spawnname}", {defaultPrice}, "{resource.category}")
    END
